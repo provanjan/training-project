ceed9df492478151cbf3f9e573d7d2817409145b
Ray <provanjan.ray@capgemini.com>
2023-05-16 11:21:32 +0530

    Fifth Commit

---
+++ core/src/main/java/com/trainingproject/core/models/LogoModel.java
@@ -0,0 +1,33 @@
+package com.trainingproject.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Optional;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Model(adaptables = Resource.class)
+public class LogoModel {
+    @Inject
+    @Optional
+    @Named("logoUrl")
+    private String imageUrl;
+
+    @Inject
+    @Optional
+    @Named("altText")
+    private String altImgText;
+
+    public void init() {
+//Business logic or any other initialization will come here
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getAltImgText() {
+        return altImgText;
+    }
+}
